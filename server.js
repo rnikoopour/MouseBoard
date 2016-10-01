7b102df5d28e22ae6597afde5d2cdbaa8bbdb48a
Dhb Dhb <dhbdhb@Dhbs-MacBook-Pro.local>
2016-10-01 12:17:28 -0700

    Debugging keyboard

--- server.js
+++ server.js
@@ -42,12 +42,31 @@ app.listen(port, listenOn, () => {
 const keyTransformer = {
     Backspace: 'backspace',
     Meta: 'command',
-}
+    Alt: 'alt',
+    Shift: 'Shift',
+    Control: 'control',
+    F1: 'f1',
+    F2: 'f2',
+    F3: 'f3',
+    F4: 'f4',
+    F5: 'f5',
+    F6: 'f6',
+    F7: 'f7',
+    F8: 'f8',
+    F9: 'f9',
+    F10: 'f10',
+    F11: 'f11',
+    F12: 'f12',
+    ArrowUp: 'up',
+    ArrowDown: 'down',
+    ArrowRight: 'right',
+    ArrowLeft: 'left',
+    Enter: 'enter'
+};
 
 function keyboard({key, modifierKeys}) {
     const tranformedKey = keyTransformer[key] || key;
     robot.keyTap(tranformedKey);
-    // Doing this cuz it gets stuck on cmd + tab
 }
 
 function click({btn, dblClick}) {
